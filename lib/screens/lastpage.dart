f8cff3a3d0990519beecc1ce151f62b7a11137f3
Anfal <ahassan2@mof.gov.kw>
2022-12-28 15:30:48 +0300

    last page

---
+++ lib/screens/lastpage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:fuel_ur_way_frontend/style/colors.dart';
+
+import 'package:fuel_ur_way_frontend/screens/home.dart';
+
+class LastScreen extends StatelessWidget {
+  const LastScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Image(
+                  image: AssetImage('assets/images/smalllogo.png'),
+                  fit: BoxFit.contain,
+                  height: 60,
+                  width: 160,
+                ),
+                Image(
+                  image: AssetImage('assets/images/name.png'),
+                  fit: BoxFit.contain,
+                  height: 120,
+                  width: 260,
+                ),
+              ],
+            ),
+          ),
+          Text(
+            'Thank you we received your  order... we will be with you shortly',
+            style: TextStyle(fontSize: 30, color: drawerBackgroundColor),
+          ),
+          Container(
+            width: 250,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () async {
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) => MainScreen()));
+              },
+              child: Text(
+                "Done",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
+              ),
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                backgroundColor: MaterialStateProperty.all(buttonColor),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
